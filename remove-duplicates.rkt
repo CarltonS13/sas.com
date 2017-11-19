d007b46b9f5da9c9efe5588050e52dd135a255df
MorizSchildorfer <MorizSchildorfer@users.noreply.github.com>
2017-11-19 19:09:44 +0100

    AddedTestingGenerationProcedures
    Still lacking documentation

--- remove-duplicates.rkt
+++ remove-duplicates.rkt
@@ -1,12 +1,28 @@
 #lang racket
 (require csc151)
-
-(define la '(("Moore" "Emily" "emoore" "4205" "MAT" "Emeritus" "CSC")
-             ("Moore" "Emily" "emoore" "4205" "MAT" "Emeritus" "CSC")
-             ("Moore" "Tom" "tmoore" "0000" "Statistics" "Emeritus" "MAT")
-             ("Moore" "Orless" "punny" "1234" "CSC" "Emeritus" "Fictitious" "NSF Grant")
-  
-             ("Moore" "Tom" "tmoore" "0000" "Statistics" "Emeritus" "MAT")))
+;Purpose Create a string out of a random number in range 0-int exclusive
+(define rand-string-num
+  (lambda (int)
+    (number->string (random int))))
+(define list-of-list-of-rans
+  (lambda (count int rand)
+    (let kernel ([to-go count]
+                 [so-far (list)])
+      (cond
+        [(zero? to-go)
+         so-far]
+        [else
+         (kernel (decrement to-go) (cons (list-of-rans int rand) so-far))]))))
+(define list-of-rans
+  (lambda (int rand)
+    (let kernel ([to-go int]
+                 [so-far (list)])
+      (cond
+        [(zero? to-go)
+         so-far]
+        [else
+         (kernel (decrement to-go) (cons (rand-string-num rand) so-far))]))))
+(define utter-mess (list-of-list-of-rans 2000 10 2000))
 
 ;;; Procedure
 ;;;   filter-duplicates
