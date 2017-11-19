568c2a1e5c0d6f241d06a906dbc407a74b92e2ab
CarltonS13 <carltonsegbefia@gmail.com>
2017-11-19 11:45:16 -0600

    Added documentation

--- Final Project.rkt
+++ Final Project.rkt
@@ -15,6 +15,144 @@
 ;;; Postconditions:
 ;;;   
 ;;;   
+;;;
+
+
+;;; Procedure:
+;;;   filter-duplicates
+;;; Parameters:
+;;;   parameters
+;;; Purpose:
+;;;   purpose
+;;; Produces:
+;;;   product
+;;; Preconditions:
+;;;  
 ;;;   
+;;; Postconditions:
+;;;   
+;;;   
+;;;
+
+
+;;; Procedure:
+;;;   clean-data 
+;;; Parameters:
+;;;   lst, a list 
+;;; Purpose:
+;;;   Remove the fields that we don’t want to include
+;;; Produces:
+;;;   product
+;;; Preconditions:
+;;;  
+;;;   
+;;; Postconditions:
+;;;   
+;;;   
+;;;
+
+
+;;; Procedure:
+;;;   split-data
+;;; Parameters:
+;;;   parameters
+;;; Purpose:
+;;;   purpose
+;;; Produces:
+;;;   product
+;;; Preconditions:
+;;;  
+;;;   
+;;; Postconditions:
+;;;   
+;;;   
+;;;
+
+;;; Procedure:
+;;;   weighted-values
+;;; Parameters:
+;;;   parameters
+;;; Purpose:
+;;;   takes a lst and returns the sum after multipling the weights
+;;; Produces:
+;;;   product
+;;; Preconditions:
+;;;  
+;;;   
+;;; Postconditions:
+;;;   
+;;;   
+;;;
+
+(define weights (vector 1 1 1 1 1 1 1 1 1 1))
+
+;;; Procedure:
+;;;   optimize weights 
+;;; Parameters:
+;;;   parameters
+;;; Purpose:
+;;;   purpose
+;;; Produces:
+;;;   product
+;;; Preconditions:
+;;;  
+;;;   
+;;; Postconditions:
+;;;   
+;;;   
+;;;
+
+
+;;; Procedure:
+;;;   check-prediction
+;;; Parameters:
+;;;   parameters
+;;; Purpose:
+;;;   purpose
+;;; Produces:
+;;;   product
+;;; Preconditions:
+;;;  
+;;;   
+;;; Postconditions:
+;;;   
+;;;   
+;;;
+
+
+;;; Procedure:
+;;;   predict
+;;; Parameters:
+;;;   parameters
+;;; Purpose:
+;;;   purpose
+;;; Produces:
+;;;   product
+;;; Preconditions:
+;;;  
+;;;   
+;;; Postconditions:
+;;;   
+;;;   
+;;;
+
+;;; Procedure:
+;;;   check-fails
+;;; Parameters:
+;;;   weights
+;;;   deviance
+;;;   list
+;;; Purpose:
+;;;   purpose
+;;; Produces:
+;;;   product
+;;; Preconditions:
+;;;  
+;;;   
+;;; Postconditions:
+;;;   
+;;;   
+;;;
+
 
 
