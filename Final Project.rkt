b31b3ff02484b43b1f1bbe214a26b92937f461f8
CarltonS13 <carltonsegbefia@gmail.com>
2017-11-19 10:56:48 -0600

    Intitial
    Created a racket file and added a sample documentation stub

---
+++ Final Project.rkt
@@ -0,0 +1,20 @@
+#lang racket
+(require csc151)
+
+;;; Procedure:
+;;;   procedure-name
+;;; Parameters:
+;;;   parameters
+;;; Purpose:
+;;;   purpose
+;;; Produces:
+;;;   product
+;;; Preconditions:
+;;;  
+;;;   
+;;; Postconditions:
+;;;   
+;;;   
+;;;   
+
+
